337bb1ce964f00bd96ec99f4c538f48d686867b7
talpx.work@gmail.com <talpx.work@gmail.com>
2023-09-17 02:49:00 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/talpx0/tiiio
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+type array[T, U any] []T
+
+func (arr *array[T,U]) reduce(initial U, reducer func(U, T) U) U {
+	acc := initial
+	for _, item := range *arr {
+		acc = reducer(acc, item)
+	}
+	return acc
+}
+
+
+func (arr *array[T, U]) mapping(iterator func(T) U ) *[]U {
+	result := []U{}
+	for _, item := range *arr {
+		result = append(result, iterator(item))
+	}
+	return &result
+}
+
+
